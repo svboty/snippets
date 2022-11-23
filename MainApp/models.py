e9e3583a28d9f84b7266d698636b18efb9965d4b
Sergey.Botvinko <sergey.botvinko>
2022-11-23 16:23:42 +0700

    migrate for new attr

--- MainApp/models.py
+++ MainApp/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -6,3 +7,6 @@ class Snippet(models.Model):
     lang = models.CharField(max_length=30)
     code = models.TextField(max_length=5000)
     creation_date = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    public = models.BooleanField('Публичный', default=False)
+
