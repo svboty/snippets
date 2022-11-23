132cd4080a2faef2e1c4e35bb2a069c2608cf222
Sergey.Botvinko <sergey.botvinko>
2022-11-23 15:49:59 +0700

    migrate

--- Snippets/urls.py
+++ Snippets/urls.py
@@ -5,6 +5,7 @@ from MainApp import views
 
 urlpatterns = [
     path('', views.index_page),
-    path('snippets/add', views.add_snippet_page),
-    path('snippets/list', views.snippets_page),
+    path('snippets/add', views.add_snippet_page, name='snippets-add'),
+    path('snippets/list', views.snippets_page, name='snippets-list'),
+    path('snippets/<int:id>', views.snippets_page, name='snippets-detail'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
